da29a7ee0259ad721c9e2c9864770c4d92476187
Kylo95 <kyle_bain@live.com>
2017-04-05 21:25:18 +0200

    Consolidated student search inti student details page

--- website/templates/StudentDetailstemp.php
+++ website/templates/StudentDetailstemp.php
@@ -34,7 +34,26 @@
 <?php
 													
 }
+else
+	echo('<p> </p>');
 
 
 ?>	
 		
+			<div id="dynamicBox" style="width: 648px; height: 480px;">
+				<br />
+				<br />
+				<h2>Student Search</h2>
+				<br />
+				<form action="StudentDetails" method="get">
+					Student ID<br />
+					<input type="text" name="studentid" style="width: 250px;"><br /><br />
+					
+					<input type="submit" value="Search" style="width: 250px; height: 40px;">
+				</form>
+			</div>
+		</div>
+	</body>
+</html>
+
+?>
