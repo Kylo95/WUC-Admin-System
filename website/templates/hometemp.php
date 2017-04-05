fe8cd263c4a04fac52b0cf6cfd22c6696202d850
Kylo95 <kyle_bain@live.com>
2017-04-05 19:40:31 +0200

    Changed login/logout system
    1. Removed logintemp.php page, moved relevant code to hometemp.php
    2. Fixed blank screen on login details input errors
    3. Added error message if incorrect details entered for login
    4. Fixed buttons hyperlinks to include whole button for sidebar
    5. Logout redirects to home instead of requiring hope page php template.

--- website/templates/hometemp.php
+++ website/templates/hometemp.php
@@ -1,12 +1,44 @@
-
+<?php
+	/*starts the session to determine which page is selected upon log in*/
+	$error = 1;
+	if (!empty($_POST['staff_id']) || !empty($_POST['password']))
+	{	
+		$staffTable = new DatabaseTable($pdo, 'staff');
+		$results = $staffTable->find($_POST);	//selecting all information from new accounts where the username and password match what's entered to provide log in//
+		
+		foreach($results as $rows)
+		{
+			$error = 0;
+			if ($rows['loa'] == 2){
+				$_SESSION['user'] = $rows['staff_id']; //setting the firstname if the user so there name can be displayed //
+				$_SESSION['priv'] = 2;			/*if user is set to 2 within access then will link as an admin and allow access to higher access functions*/
+			}
+			else{
+				$_SESSION['user'] = $rows['staff_id'];
+				$_SESSION['priv'] = 1;			/*if user is set to 1 within access then wilol links as a user to generic page with less access*/
+			}
+		}
+		
+		if($error)
+			echo("<p>No account found, please make sure you have provided the right information.</p>");
+	}
+	
+	/*check if already logged in and show welcome*/
+	if(isset($_SESSION['user']))
+		require 'Welcometemp.php';
+	else if($error)
+	{
+	
+	/*show login if not logged in or error with details*/
+?>
 			<div id="dynamicBox" style="width: 648px; height: 480px;">
 				<br />
 				<br />
 				<h2>Login</h2>
 				<br />
-				<form action="login" method="post">
+				<form action="" method="post">
 					Staff Id<br />
-					<input type="text" name="staffid" style="width: 250px;"><br /><br />
+					<input type="text" name="staff_id" style="width: 250px;"><br /><br />
 					Password<br />
 					<input type="password" name="password" style="width: 250px;"><br /><br />
 					<input type="submit" value="Login" style="width: 250px; height: 40px;">
@@ -15,3 +47,6 @@
 		</div>
 	</body>
 </html>
+<?php
+	}
+?>
